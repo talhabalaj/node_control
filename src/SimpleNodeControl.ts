b6e366def0c987aa0fa8e923344bceae0b9a8864
Talha Balaj <talhabalaj@hotmail.com>
2021-12-04 15:13:41 +0500

    Added SimpleNodeControl

---
+++ src/SimpleNodeControl.ts
@@ -0,0 +1,21 @@
+import { SSHExecCommandResponse } from "node-ssh";
+import NodeControl, { NodeSystemStats } from "./NodeControl";
+import { SystemDResult } from "./parser";
+
+export default class SimpleNodeControl extends NodeControl {
+  getSystemStats(...args: any[]): Promise<NodeSystemStats> {
+    throw new Error("Method not implemented.");
+  }
+  installServer(): Promise<SSHExecCommandResponse> {
+    throw new Error("Method not implemented.");
+  }
+  getServiceStatus(): Promise<SystemDResult> {
+    throw new Error("Method not implemented.");
+  }
+  restartServer(): Promise<SSHExecCommandResponse> {
+    throw new Error("Method not implemented.");
+  }
+  stopServer(): Promise<SSHExecCommandResponse> {
+    throw new Error("Method not implemented.");
+  }
+}
