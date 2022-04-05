4dcc897adc9796000de8c9b4d3b9f1a9eab196e7
Talha Balaj <talhabalaj@hotmail.com>
2022-04-05 20:16:11 +0500

    Updated proxy script

--- src/scripts/install_proxy.sh
+++ src/scripts/install_proxy.sh
@@ -1,60 +1,44 @@
 #!/bin/bash
 #
 # Name:		install-VezGate-Proxy.sh
-# Version:	1
+# Version:	2
 # Author:	umar
 # Purpose:	(For Cloud Servers) Sets up proxy server for Apps
 # Usage:	bash install-vpn-server.sh
 #################################################################################################
 
-# read -p "Enter Domain For This Proxy: " domain
-if [ -z $domain ] ; then
-    echo 'No Domain Given, Exiting' >&2
-    exit 1
+if [ -z $domain ]; then
+  echo 'No Domain Given, Exiting' >&2
+  exit 1
 fi
-# read -p "Enter Git Username: " VGPMYGITUSERNAME
-# if [ -z $VGPMYGITUSERNAME ] ; then
-#     echo 'NO GIT username given, exiting....'
-#     exit 1
-# fi
-# echo '-'
-# read -p "Enter Git Password: " VGPMYGITPASSWORD
-# if [ -z $VGPMYGITPASSWORD ] ; then
-#     echo 'NO GIT Password given, exiting....'
-#     exit 1
-# fi
 
 if ! [ -f "/etc/debian_version" ]; then
   logger -s "[error] Debian is required"
-  exit -1;
+  exit -1
 fi
 
-export DEBIAN_FRONTEND=noninteractive 
+export DEBIAN_FRONTEND=noninteractive
 
 myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 echo "Host IP is $myip"
 
 timedatectl set-timezone UTC
 apt-get -y update
-apt-get -y install build-essential git curl net-tools mtr iptraf-ng wget nload ufw certbot dnsutils
+apt-get -y install ufw certbot nginx python3-certbot-nginx curl net-tools mtr iptraf-ng wget nload dnsutils
 
 domain_ip=$(dig +short $domain)
 
-if [ -z $domain_ip ] ; then
+if [ -z $domain_ip ]; then
   echo 'No IP Found, Exiting' >&2
   exit 1
 fi
 
 echo "Domain IP is $domain_ip"
 
-if [ $domain_ip != $myip ] ; then
+if [ $domain_ip != $myip ]; then
   echo 'IP Mismatch, Exiting' >&2
   exit 1
 fi
-
-
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
-apt-get install -y nodejs
 
 ufw allow from 124.29.223.2
 ufw allow from 43.251.253.46
@@ -63,32 +47,34 @@ ufw allow from 92.204.174.192/26
 ufw allow from 92.42.105.64/28
 ufw allow from 92.42.110.56/29
 ufw allow from 134.119.185.88/29
+ufw allow from 92.204.172.192/29
+ufw allow from 151.106.6.0/28
 ufw allow http
 ufw allow https
-ufw enable
+ufw --force enable
+ufw status
 
-npm install pm2@latest -g
-pm2 update
-pm2 startup
+cat <<END >/etc/nginx/sites-available/$domain
+server {
+        listen 80;
+        server_name     $domain;
+        proxy_set_header  proxy-domain-name \$host;
+        proxy_set_header  client-real-ip \$remote_addr;
+        proxy_set_header  proxy-request-key sOmeSecReTk3Ywh1Ch15uNBr34k4bLe;
+        proxy_set_header  its-nginx-proxied 1;
 
-mkdir -p /etc/mycode
-cd /etc/mycode
 
-git clone "https://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@github.com/aahhoo/VezGate-PROXY.git"
+        location / {
+                proxy_pass http://vezcoremainapis1.rateela.com:33721;
+        }
 
-sed -i "s/REPLACE_CERT_DOMAIN_HERE/$domain/g" /etc/mycode/VezGate-PROXY/sslCertObj.js
+}
+END
 
-cd VezGate-PROXY
 
-npm i
-pm2 start ecosystem.config.js
-pm2 save
-pm2 stop all
-certbot certonly --standalone --preferred-challenges http --agree-tos --email mypublicemailidforemail123@gmail.com -d $domain
-pm2 start all
+ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/$domain
+rm -f /etc/nginx/sites-enabled/default
+certbot --nginx --redirect --agree-tos --no-eff-email --email chotasharla@gmail.com -d $domain
 
-cat > "cronfile.sh" <<EOF
-0 0 */20 * * /usr/bin/certbot renew --quiet
-EOF
-
-crontab cronfile.sh
+systemctl restart nginx
+systemctl status nginx
